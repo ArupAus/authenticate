157ec1e4964ccdfd87e96831c7dbc8da45da9e6b
Caspar Jeffrey <caspar@sol.com.au>
2019-04-12 12:36:23 +1000

    bind the simple auth to the window object

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 export { default as AuthProvider } from './AuthProvider'
 export { default as withAuth } from './withAuth'
+
+import AuthProvider, { getAuth } from './AuthProviderSimple'
+window.AuthProvider = AuthProvider
+window.getAuth = getAuth
