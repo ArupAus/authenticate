3539509c7fee75440a1c33337b83dc5cac8a90c3
Caspar Jeffrey <caspar@sol.com.au>
2019-05-14 16:09:29 +1000

    move to globalThis polyfill

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@ export { default as AuthProvider } from './AuthProvider'
 export { default as withAuth } from './withAuth'
 
 import AuthProvider, { getAuth } from './AuthProviderSimple'
-window.AuthProvider = AuthProvider
-window.getAuth = getAuth
+
+//whats this?
+//https://mathiasbynens.be/notes/globalthis
+(function() {
+	if (typeof globalThis === 'object') return;
+	Object.prototype.__defineGetter__('__magic__', function() {
+		return this;
+	});
+	__magic__.globalThis = __magic__;
+	delete Object.prototype.__magic__;
+}());
+
+globalThis.AuthProvider = AuthProvider
+globalThis.getAuth = getAuth
