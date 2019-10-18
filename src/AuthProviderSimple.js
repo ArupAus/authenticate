84d00cd2758667a9dab7f06ab96405389554c2fb
Peter Schmidt <peter@peterjs.com>
2019-10-18 14:40:03 +1100

    💥 Remove unused imports from AuthProviderSimple

--- src/AuthProviderSimple.js
+++ src/AuthProviderSimple.js
@@ -1,12 +1,7 @@
-import { Component, Children } from 'react'
-import PropTypes from 'prop-types'
 import Auth0Lock from 'auth0-lock'
-import authType from './authType'
 import EventEmitter from './EventEmitter.js'
 
 const TOKEN_KEY = process.env.AUTH_TOKEN_KEY || 'authenticate-token'
-const ACCESS_TOKEN_SUFFIX = '-access-token'
-const AUTHO_ALG = process.env.AUTH_ALG || 'RS256'
 const AUTH_NAMESPACE = 'http://authz.arup.digital/authorization'
 
 function AuthProviderSimple(info, redirect, fetchUrl) {
